aaefb6b848e46d6eba81bf2cee6304df46975950
Pranay7717 <rajanapranay987@gmail.com>
2025-05-16 18:15:23 -0500

    Update views.py

--- patents/views.py
+++ patents/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def Patients(request):
     return HttpResponse("Details of the patent will be displayed here")
+def patientshtml(request):
+    template = loader.get_template('patents.html')
+    return HttpResponse(template.render())
 
 # Create your views here.
 
