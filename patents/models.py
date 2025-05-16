864d50f832afeaeef3756c1bf36533e099aff8af
Pranay7717 <rajanapranay987@gmail.com>
2025-05-16 18:17:12 -0500

    Update models.py

--- patents/models.py
+++ patents/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class patents(models.Model):
+    firstname = models.CharField(max_length =220)
+    lastname = models.CharField(max_length = 220)
+class doctors(models.Model):
+    firstname = models.CharField(max_length =220)
+    lastname = models.CharField(max_length = 220)
+    
+    
+
 # Create your models here.
